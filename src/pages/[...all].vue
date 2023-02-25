2e9ba722a4475976ac93c0975928153e1296123c
Curtion <curtion@126.com>
2023-02-25 22:20:58 +0800

    chore: 初始样式

--- src/pages/[...all].vue
+++ src/pages/[...all].vue
@@ -1,5 +1,23 @@
+<script setup lang="ts">
+const router = useRouter()
+
+function go(e: MouseEvent) {
+  e.preventDefault()
+  router.push('/')
+}
+</script>
+
 <template>
-  <div>
-    Not Found
+  <div
+    class="left-1/2 top-1/2 translate-x--1/2 translate-y--1/2"
+    fixed
+    flex
+    flex-col
+    items-center
+    justify-center
+  >
+    404 Not Found
+    <br>
+    <a href="/" class="pt-3 hover:underline" @click="go">回到首页</a>
   </div>
 </template>
