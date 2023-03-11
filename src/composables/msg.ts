43dfb353fdfc13d6d3e4323046bc38b0e1d3ee65
Curtion <curtion@126.com>
2023-03-11 22:58:29 +0800

    chore: 验证多行页面

--- src/composables/msg.ts
+++ src/composables/msg.ts
@@ -6,7 +6,50 @@ export const msgList = ref([
   {
     send: 'Hello',
     receive: 'World',
-  }],
+  }, {
+    send: 'Hello',
+    receive: 'World',
+  },
+  {
+    send: 'Hello',
+    receive: 'World',
+  }, {
+    send: 'Hello',
+    receive: 'World',
+  },
+  {
+    send: 'Hello',
+    receive: 'World',
+  }, {
+    send: 'Hello',
+    receive: 'World',
+  },
+  {
+    send: 'Hello',
+    receive: 'World',
+  }, {
+    send: 'Hello',
+    receive: 'World',
+  },
+  {
+    send: 'Hello',
+    receive: 'World',
+  }, {
+    send: 'Hello',
+    receive: 'World',
+  },
+  {
+    send: 'Hello',
+    receive: 'World',
+  }, {
+    send: 'Hello',
+    receive: 'World',
+  },
+  {
+    send: 'Hello',
+    receive: 'World',
+  },
+],
 )
 export const sendMsg = (msg: string) => {
   msgList.value.push({
