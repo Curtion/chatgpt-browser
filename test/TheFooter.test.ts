51fe1d60edd14531fff3c0d69c8860ea4e5e3cad
Curtion <curtion@126.com>
2023-04-01 23:13:12 +0800

    chore: 测试

--- test/TheFooter.test.ts
+++ test/TheFooter.test.ts
@@ -3,20 +3,22 @@ import { describe, expect, it } from 'vitest'
 import TheFooter from '../src/components/TheFooter.vue'
 
 describe('TheFooter.vue', () => {
-  it('should render', () => {
+  it('渲染', () => {
     const wrapper = mount(TheFooter)
-    expect(wrapper.text()).toContain('2023')
+    expect(wrapper.html()).toContain('i-carbon-send')
     expect(wrapper.html()).toMatchSnapshot()
   })
 
-  it('should be interactive', async () => {
+  it('发送信息测试', async () => {
     const wrapper = mount(TheFooter)
-    expect(wrapper.text()).toContain('2023')
-
-    expect(wrapper.find('.inc').exists()).toBe(true)
-
-    await wrapper.get('button').trigger('click')
-
-    expect(wrapper.text()).toContain('1')
+    expect(wrapper.find('.i-carbon-send').exists()).toBe(true)
+    expect(msgList.value.length).toBe(0)
+    await wrapper.get('input').setValue('你好')
+    await nextTick()
+    await wrapper.get('.i-carbon-send').trigger('click')
+    await nextTick()
+    expect(wrapper.find('.i-carbon-close-outline').exists()).toBe(true)
+    expect(msgList.value.length).toBe(1)
+    expect(wrapper.html()).toMatchSnapshot()
   })
 })
